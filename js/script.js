67e07ab1b7c5e2b1ba3c4ea1bea8ab7cb386da3f
Alex <phoenix.trade2307@gmail.com>
2021-06-29 01:07:08 +0300

    добавление параметров по умолчанию и оставшиеся параметры ...rest

--- js/script.js
+++ js/script.js
@@ -155,7 +155,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // всплытие модалки по времени
 
-    // const modalTimerId = setInterval(openModal, 3000);
+    const modalTimerId = setInterval(openModal, 3000);
 
     // всплытие модалки по скролингу
 
@@ -174,12 +174,14 @@ window.addEventListener('DOMContentLoaded', () => {
     // Используем классы для карточек товара
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes; // добавляем оставшиеся параметры (...rest)
+            //при этом нам в любом случае возвращается массив (даже если он пустой)
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27; // курс валюты
             this.changeToUAH(); // при запуске этого метода у новообразованного объекта
@@ -192,16 +194,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() { // генерация html для div, который разместим потом в родительском контейнере
             const element = document.createElement('div');
+            this.classes.forEach(className => element.classList.add(className));
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
             this.parent.append(element);
@@ -227,7 +228,9 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
+        'big'
     );
     div.render();
     const div2 = new MenuCard( //нужные аргументы
@@ -236,7 +239,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         10,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     );
     div2.render();
     const div3 = new MenuCard( //нужные аргументы
@@ -245,7 +249,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         20,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     );
     div3.render();
 
